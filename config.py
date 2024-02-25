64258957f40bfa01513ee7a814ae6f1ab1fb8bba
dinuroop.__286 <78245931+Dinuroop@users.noreply.github.com>
2024-02-25 20:36:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = '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'
 FIREBASE_API_KEY = 'AIzaSyC9XW-C5-eIFN07xQ5orz_4mvTvt0P2N5s'
 FIREBASE_AUTH_DOMAIN = 'chatbot-1ba16.firebaseapp.com'
 FIREBASE_DATABASE_URL = 'https://chatbot-1ba16.firebaseio.com'
